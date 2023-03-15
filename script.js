cbbfeea7f3bd6994758888ee00fd8c529816823f
nqwzxs <nqwzxs@gmail.com>
2023-03-15 18:23:34 +0500

    Add playRound function

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3);
 
     if (randomNumber === 0) {
-        return 'rock'
+        return "rock";
     } else if (randomNumber === 1) {
-        return 'paper'
+        return "paper";
     } else {
-        return 'scissors'
+        return "scissors";
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats Rock";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beat paper";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats Scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beat paper";
+    } else {
+        return "It's a tie!";
+    }
+}
+
+console.log(playRound(computerSelection, playerSelection))
