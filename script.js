345b71ecc459bd1c0cab15712f23e1f127a05598
nqwzxs <nqwzxs@gmail.com>
2023-03-15 18:02:32 +0500

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return 'rock'
+    } else if (randomNumber === 1) {
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
+}
+
+console.log(getComputerChoice())
